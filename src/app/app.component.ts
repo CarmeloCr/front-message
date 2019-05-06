801a5da7e3fa09044314fece1bddf418af5c262e
carmelo <ccr376@gmail.com>
2019-05-05 20:30:38 -0500

    proyecto envio de mensajes con angular-spring

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'Bienvenido al curso de angular 7 con spring 5';
+  autor:string = 'Carmelo Cruz López'
+}
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,12 @@
+import { Cliente } from './cliente'; 
+
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: 'Carmelo', apellido: 'Cruz', email: 'ccr376@gmail.com', createAt: '02-04-2017'},
+  { id: 2, nombre: 'Jose', apellido: 'Cruz', email: 'ccr376@gmail.com', createAt: '02-04-2017'},
+  { id: 3, nombre: 'Pablo', apellido: 'Cruz', email: 'ccr376@gmail.com', createAt: '02-04-2017'},
+  { id: 4, nombre: 'Carlos', apellido: 'Cruz', email: 'ccr376@gmail.com', createAt: '02-04-2017'},
+  { id: 5, nombre: 'Uriel', apellido: 'Cruz', email: 'ccr376@gmail.com', createAt: '02-04-2017'},
+  { id: 6, nombre: 'Andres', apellido: 'Cruz', email: 'ccr376@gmail.com', createAt: '02-04-2017'},
+  { id: 7, nombre: 'Joel', apellido: 'Cruz', email: 'ccr376@gmail.com', createAt: '02-04-2017'},
+  { id: 8, nombre: 'Bertoni', apellido: 'Cruz', email: 'ccr376@gmail.com', createAt: '02-04-2017'},
+];
